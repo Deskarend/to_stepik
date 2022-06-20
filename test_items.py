8827f7f4a608bdd537b52729c0984481bcf127fb
Deskarend <107473473+Deskarend@users.noreply.github.com>
2022-06-20 17:44:59 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 
-def test_add_to_shopping_cart(browser):
+def test_button_add_to_shopping_cart_is_displayed(browser):
     browser.get( "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(30)
     assert browser.find_element(By.CLASS_NAME,"btn-add-to-basket")
