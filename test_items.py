71f03333149244fdacc00998be54fefd0a5dfc0d
Deskarend <107473473+Deskarend@users.noreply.github.com>
2022-06-20 17:04:06 +0300

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+
+def test_add_to_shopping_cart(browser):
+    browser.get( "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(30)
+    assert browser.find_element(By.CLASS_NAME,"btn-add-to-basket")
+    
